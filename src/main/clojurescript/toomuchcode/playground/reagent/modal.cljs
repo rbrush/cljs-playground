082eed39ce663e24ec03f3e2997884494148d4c8
Ryan Brush <rbrush@gmail.com>
2014-08-27 22:09:12 -0500

    Refactor Reagent modal experiment to just generate bootstrap-friendly HTML directly.

--- src/main/clojurescript/toomuchcode/playground/reagent/modal.cljs
+++ src/main/clojurescript/toomuchcode/playground/reagent/modal.cljs
@@ -5,45 +5,49 @@
 
 (def person-detail-state (atom {:person-details nil}))
 
+(defn modal
+  "Returns a component representing a model."
+  [{:keys [title content footer close-fn]}]
+  [:div
+   [:div.modal-backdrop.fade.in]
+   [:div.modal.fade.in {:tabIndex "-1" :role "dialog" :style {:display "block"} :on-click close-fn}
+    [:div.modal-dialog
+     [:div.modal-content
+      [:div.modal-header
+       [:h4.modal-title title]]
+      content
+      [:div.modal-footer
+       footer
+       [:div.btn.btn-default {:type "button"
+                              :on-click close-fn}
+        "Close"]]]]]])
+
 (defn person-details-modal []
-  (let [{:keys [person-details on-person-change]} @person-detail-state]
+ (let [{:keys [person-details on-person-change]} @person-detail-state
+       close-fn #(swap! person-detail-state assoc :person-details nil)]
 
-    [:div
+   ;; Render modal only when there are details set.
+   (if (nil? person-details)
+     [:div]
+     (modal
+      {:title "Person Info"
+       :content [:div (str (:name person-details) " lives in " (:city person-details))]
+       :footer (if (:vip person-details )
+                 [:div.btn.btn-primary {:type "button"
+                                        :on-click (fn []
+                                                    (on-person-change (assoc person-details :vip false))
+                                                    (close-fn))}
+                  "Unmark as VIP"]
+                 [:div.btn.btn-primary {:type "button"
+                                        :on-click (fn []
+                                                    (on-person-change (assoc person-details :vip true))
+                                                    (close-fn))}
+                  "Mark as VIP"])
 
-     ;; Render modal only when there are details set.
-     (when (not (nil? person-details))
-       (js/ReactBootstrap.Modal
-        #js {:title "Person info"
-             :animated false
-             :onRequestHide #(swap! person-detail-state assoc :person-details nil)}
-
-        (reagent/as-component [:div (str (:name person-details) " lives in " (:city person-details))])
-
-        (reagent/as-component
-         [:div.modal-footer
-
-          (if (:vip person-details)
-            (js/ReactBootstrap.Button
-             #js {:bsStyle "primary"
-                  :onClick (fn []
-                             (on-person-change (assoc person-details :vip false))
-                             (swap! person-detail-state assoc :person-details nil))}
-
-             "Unmark as VIP")
-
-            (js/ReactBootstrap.Button
-             #js {:bsStyle "primary"
-                  :onClick (fn []
-                             (on-person-change (assoc person-details :vip true))
-                             (swap! person-detail-state assoc :person-details nil))}
-
-             "Mark as VIP"))
-
-          (js/ReactBootstrap.Button #js {:onClick #(swap! person-detail-state assoc :person-details nil)}
-                                    "Close")])))]))
+       :close-fn close-fn}))))
 
 (def playground-state (atom {:people [{:name "Alice" :city "Kansas City" :vip false}
-                                      {:name "Bob" :city "St. Louis" :vip false}
+                                      {:name "Robert" :city "St. Louis" :vip false}
                                       {:name "Charles" :city "Chicago" :vip false}]}))
 
 (defn show-person-details-modal!
@@ -54,18 +58,19 @@
 (defn playground []
   [:div "Hello, reagent modal world!"
    [:div
-    (apply js/ReactBootstrap.ButtonGroup
-           #js {:className "btn-group-vertical"}
-           (map-indexed
-            (fn [index {:keys [name] :as person}]
-              (js/ReactBootstrap.Button #js
-                                        {:bsStyle (if (:vip person) "danger" "default")
-                                         :onClick #(show-person-details-modal!
-                                                    person
-                                                    (fn [updated] (swap! playground-state assoc-in [:people index] updated )))}
-                                        name))
-
-            (get  @playground-state :people)))]])
+    [:div.btn-group.btn-group-vertical
+     (map-indexed
+      (fn [index {:keys [name vip] :as person}]
+        ^{:key index}
+        [:button.btn
+         {:class (if vip "btn-danger" "btn-default")
+          :type "button"
+          :on-click
+          (fn [] (show-person-details-modal!
+                 person
+                 (fn [updated] (swap! playground-state assoc-in [:people index] updated ))))}
+         name])
+      (get @playground-state :people))]]])
 
 (defroute "/reagent/modal" []
   (reagent/render-component [playground]
